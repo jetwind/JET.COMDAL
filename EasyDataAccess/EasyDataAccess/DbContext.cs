fd261c66dc3ad9016a78dd3524cf7adda1e08be0
jetwind <jetwindlucky@gmail.com>
2014-06-11 17:58:45 +0800

    Create Sln
    新建了项目并定义了接口

---
+++ EasyDataAccess/EasyDataAccess/DbContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace EasyDataAccess
+{
+    public class DbContext<T>:IOperationAble<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProviderName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbCommand _DbCommand { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DbConnection _DbConnection { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="providerName">数据驱动名称</param>
+        public DbContext(string providerName)
+        {
+            this.ProviderName = providerName;
+            DbProviderFactory dbProviderFactory= DbProviderFactories.GetFactory(providerName);
+            if (dbProviderFactory == null)
+            {
+                throw new Exception("创建DbProvider失败，请确认DbProviderName正确且存在！");
+            }
+            _DbConnection = dbProviderFactory.CreateConnection();
+            _DbCommand = dbProviderFactory.CreateCommand();
+
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int ExecuteNoQuery()
+        {
+            
+            _DbConnection.Open();
+            
+            _DbConnection.Close();
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<T> Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public System.Data.DataTable QueryTable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EasyDataAccess/EasyDataAccess/IOperationAble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace EasyDataAccess
+{
+    /// <summary>
+    /// 操作接口定义
+    /// </summary>
+    public  interface  IOperationAble<T>
+    {
+        /// <summary>
+        /// 执行无查询
+        /// </summary>
+        /// <returns></returns>
+        int ExecuteNoQuery();
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <returns></returns>
+        List<T> Query();
+
+
+        /// <summary>
+        /// 返回查询结构
+        /// </summary>
+        /// <returns></returns>
+        DataTable QueryTable();
+
+
+    }
+}
